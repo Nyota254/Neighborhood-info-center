3147c07d6c0b2365c26d7e263fc2f9048dc731af
Nyota254 <knyota66@gmail.com>
2019-10-26 16:36:47 +0300

    create model forms for the app

---
+++ main/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import (
+    Neighborhood,
+    Neighborhood_buisnesses,
+    Neighborhood_announcement,
+    Neighborhood_stories,
+    Neighborhood_contact_info,
+)
+
+class NeighborhoodCreationForm(forms.ModelForm):
+    '''
+    Form for creating neighbourhood
+    '''
+    class Meta:
+        model = Neighborhood
+        fields = ["name","location"]
+
+class NeighborhoodAnnouncementForm(forms.ModelForm):
+    '''
+    Form for creatinga announcements
+    '''
+    class Meta:
+        model = Neighborhood_announcement
+        fields = ["title","announcement"]
+
+class NeighborhoodBuisnessesForm(forms.ModelForm):
+    '''
+    Form for advertising a a buisness
+    '''
+    class Meta:
+        model = Neighborhood_buisnesses
+        fields = ["buisness_name","buisness_email"]
+
+class NeighborhoodContactForm(forms.ModelForm):
+    '''
+    Form for adding contacts to the neighbourhood
+    '''
+    class Meta:
+        models = Neighborhood_contact_info
+        fields = ["department","contact_number","contact_email"]
+
+class NeighborhoodStoryForm(forms.ModelForm):
+    '''
+    Form for creating stories about the neighbourhood
+    '''
+    class Meta:
+        models = Neighborhood_stories
+        fields = ["title","headline","story"]
+
+    
