71327e9c2cd197df2c7254372ea88c495110a2bd
Nyota254 <knyota66@gmail.com>
2019-10-26 09:23:04 +0300

    add the user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    '''
+    class for user profiles
+    '''
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    id_number = models.IntegerField()
+    profile_picture = models.ImageField(default="default.jpg",upload_to='profile_pics')
+    bio = models.TextField()
+    ############
+    # TO-DO   neibourhood f-key
+    ############
+    # neighborhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
